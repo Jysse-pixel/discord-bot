2104215c128b7883ec6db74caa8bc25d9b9dd4d9
Jysse-pixel <80070244+Jysse-pixel@users.noreply.github.com>
2021-03-04 22:11:12 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js");
+
+const Client = new Discord.Client;
+
+
+Client.on("ready", () => {
+    console.log("Bot Prêt");
+
+});
+
+Client.login("ODE3MDYyMzk5MDU1ODg4NDE1.YEECqA.NolNsjUXiU3KiWJZg5ucsT3aGFs");
