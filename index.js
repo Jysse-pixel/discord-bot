2e175525a96dacadc9444593f4dc981523b0cdf4
Jysse-pixel <80070244+Jysse-pixel@users.noreply.github.com>
2021-03-06 17:49:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,16 +10,16 @@ Client.on("ready", () => {
 
 });
 
-Client.on('ready', () => {
-    Client.user.setStatus("online")
-    Client.user.setActivity('Servir Paul Prince')
-});
-
 Client.on('guildMemberAdd', guildMember =>{
     let welcomeRole = guildMember.guild.roles.cache.find(role => role.name === '817098925479559249');
 
     guildMember.roles.add(welcomeRole);
     guildMember.guild.channels.cache.get('817421806076035072').send(`Welcome <@${guildMember.user.id}> to our server! Make sure to check out the rules channel!`)
+});
+
+Client.on('ready', () => {
+    Client.user.setStatus("online")
+    Client.user.setActivity('Servir Paul Prince')
 });
 
 Client.login(process.env.TOKEN);
