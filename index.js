af63bd8b47fc052674442f452184d6cd5968a015
Jysse-pixel <80070244+Jysse-pixel@users.noreply.github.com>
2021-03-06 17:45:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ Client.on('ready', () => {
     Client.user.setActivity('Servir Paul Prince')
 });
 
-Client.on('guildMemberAdd', member => {
-    member.guild.channels.cache.get('817421806076035072').send(`${member}`, new Discord.MessageEmbed()
-        .setDescription(`${member} a rejoint le serveur. Nous sommes désormais ${member.guild.memberCount} ! 🎉`)
-        .setColor('#00ff00'))
-    member.roles.add('817098925479559249')
-})
+client.on('guildMemberAdd', guildMember =>{
+    let welcomeRole = guildMember.guild.roles.cache.find(role => role.name === '817098925479559249');
+
+    guildMember.roles.add(welcomeRole);
+    guildMember.guild.channels.cache.get('817421806076035072').send(`Welcome <@${guildMember.user.id}> to our server! Make sure to check out the rules channel!`)
+});
 
 Client.login(process.env.TOKEN);
