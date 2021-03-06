80f674b98cd014a233855ef5b01b3b0e7d368764
Jysse-pixel <80070244+Jysse-pixel@users.noreply.github.com>
2021-03-06 18:18:35 +0100

    Add files via upload

---
+++ welcome.js
@@ -0,0 +1,14 @@
+module.exports = (Client) => {
+    const channelId = "817421806076035072";
+    client.on("guildMemberAdd", (member) => {
+      console.log(member);
+  
+      const message = `Welcome <@${
+        member.id
+      }> to our server! Be sure to check out our ${member.guild.channels.cache
+        .toString()}`;
+  
+      const channel = member.guild.channels.cache.get(channelId);
+      channel.send(message);
+    });
+  };
