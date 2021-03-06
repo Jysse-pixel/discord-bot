efffbbcf8d2d1460e0d7f719196d397fc83cbee0
Jysse-pixel <80070244+Jysse-pixel@users.noreply.github.com>
2021-03-06 17:16:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ Client.on('ready', () => {
 });
 
 Client.on('guildMemberAdd', member => {
-    member.guild.channels.cache.get('817421806076035072').send(`${member} a rejoint le serveur de la Famille Prince. Nous sommes désormais ${member.guild.memberCount} ! 🎉`)
-    member.roles.add('817082780319744030')
+    member.guild.channels.cache.get("817421806076035072").send(`${member} a rejoint le serveur de la Famille Prince. Nous sommes désormais ${member.guild.memberCount} ! 🎉`)
+    member.roles.add("817082780319744030")
 })
 
 Client.login(process.env.TOKEN);
