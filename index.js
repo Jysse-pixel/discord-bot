6bb5f6ac2c4d28df48e0abda9cd2aedd8802c7da
Jysse-pixel <80070244+Jysse-pixel@users.noreply.github.com>
2021-03-07 21:44:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ Client.on('guildMemberAdd', (member) => {
     .setDescription(`\`${member.username}\` a rejoint le serveur de la Famille Prince ! 🎉`)
     .setColor("BLUE")
     .setTimestamp()
-    .Client.channel.cache.get(channelID).send(embed)
+    bot.channels.cache.get(channelID).send(embed)
     member.roles.add('817098925479559249')
 })
 
