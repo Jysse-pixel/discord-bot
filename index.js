09028b8b504588431d5c01b6d15d20f1628e12c9
Jysse-pixel <80070244+Jysse-pixel@users.noreply.github.com>
2021-03-07 19:45:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ Client.on("ready", () => {
     console.log("Bot Prêt");
 });
 
+Client.on('guildMemberAdd', member => {
+    member.guild.channels.cache.get('817421806076035072').send(`${member} a rejoint le serveur. Nous sommes désormais ${member.guild.memberCount} ! 🎉`)
+    member.roles.add('817098925479559249')
+})
+ 
+Client.on('guildMemberRemove', member => {
+    member.guild.channels.cache.get('817421806076035072').send(`${member.user.tag} a quitté le serveur... 😢`)
+})
+
 Client.on('ready', () => {
     Client.user.setStatus("online")
     Client.user.setActivity('Servir Paul Prince')
