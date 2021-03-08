b2db9c25b504839418eb25883132a806d1fad408
Jysse-pixel <80070244+Jysse-pixel@users.noreply.github.com>
2021-03-08 18:49:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const Discord = require("discord.js");
 
 const bot = new Discord.Client();
@@ -18,11 +17,11 @@ bot.on('guildMemberAdd', (member) => {
     .setTimestamp()
     bot.channels.cache.get(channelID).send(embed)
     member.roles.add('817098925479559249')
-})
+});
 
 bot.on('ready', () => {
     bot.user.setStatus("online")
     bot.user.setActivity('Servir Paul Prince')
-})
+});
 
 bot.login(process.env.TOKEN);
