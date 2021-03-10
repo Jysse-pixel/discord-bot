cc1d96b6087b59987c4da229d7fe0d60bcfbe6a0
Jysse-pixel <80070244+Jysse-pixel@users.noreply.github.com>
2021-03-10 13:34:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -170,41 +170,10 @@ bot.on('message', message => {
         let Response = responses[Math.floor(Math.random()*(responses.length)-1)]
         let embed = new Discord.MessageEmbed()
         .setTitle(`Roll`)
-        .setDescription(`Tu as roulé ${Response}`)
+        .setDescription(`Tu as roulé **${Response}**`)
         .setColor("BLUE")
         message.channel.send(embed)
     }
 });
-
-bot.on('message', async message => {
-    if(message.author.bot) return
-    if(message.content.startsWith(PREFIX)) return
-
-    const args = message.content.substring(PREFIX.length).split(" ")
-
-    if(message.content.startsWith(`${PREFIX}play`)) {
-        const voiceChannel = message.member.voice.channel
-        if(!voiceChannel) return message.channel.send('Tu dois être dans un channel pour mettre de la musique.')
-
-        try {
-            var connection = await voiceChannel.join()
-        } catch (error) {
-            console.log(`Il y a une erreur dans la connection au channel : ${error}`)
-            return message.channel.send(`Il y a une erreur dans la connection au channel : ${error}`)
-        }
-        
-        const dispatcher = connection.play(ytdl(args[1]))
-        .on('finish', () => {
-            voiceChannel.leave()
-        })
-        .on('error', error => {
-            console.log(error)
-        })
-        dispatcher.setVolumeLogarithmic(5 / 5)
-    } else if (message.content.startsWith(`${PREFIX}stop`)) {
-        if (!message.member.voice.channel) return message.channel.send('Tu dois être dans le channel pour stopper la musique')
-        return undefined
-    }
-    });
-    
+  
 bot.login(process.env.TOKEN);
